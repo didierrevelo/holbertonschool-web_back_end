49feada5d8df1f3488d89866870b63d177b8af4c
didierrevelo <1829@holbertonschool.com>
2021-10-20 12:46:52 -0500

    0x02-python_async_comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -15,5 +15,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension():
+    """taks 1"""
     a = [i async for i in async_generator()]
     return a
