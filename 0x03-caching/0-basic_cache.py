5187a51e0ebe5bf76a0bb0616eaf3c61df349bf8
didierrevelo <1829@holbertonschool.com>
2021-10-24 22:54:20 -0500

    0-basic_cache.py

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -20,11 +20,11 @@ from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """basic dictionary"""
+    """basic dictionary put item in cache"""
     def put(self, key, item):
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """get"""
+        """get item from cache"""
         return self.cache_data.get(key, None)
