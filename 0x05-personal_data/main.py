9a1429f7ddf4235bd1e032c5cda1c046c90dd784
didierrevelo <1829@holbertonschool.com>
2021-11-01 22:42:02 -0500

    filtered_logger.py

--- 0x05-personal_data/main.py
+++ 0x05-personal_data/main.py
@@ -3,12 +3,12 @@
 Main file
 """
 
-import logging
-import re
+get_db = __import__('filtered_logger').get_db
 
-RedactingFormatter = __import__('filtered_logger').RedactingFormatter
-
-message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
-log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
-formatter = RedactingFormatter(fields=("email", "ssn", "password"))
-print(formatter.format(log_record))
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()
