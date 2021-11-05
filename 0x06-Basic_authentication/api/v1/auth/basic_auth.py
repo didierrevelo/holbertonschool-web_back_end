684a2e3e387c90a449505fb1749ec49304ef8f79
didierrevelo <1829@holbertonschool.com>
2021-11-04 23:23:10 -0500

    api/v1/auth/basic_auth.py

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -14,11 +14,22 @@ import BasicAuth from api.v1.auth.basic_auth
 create an instance of BasicAuth
 and assign it to the variable auth
 Otherwise, keep the previous mechanism
-with auth an instance of Auth."""
+with auth an instance of Auth.
+"""
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
     """BasicAuth class"""
 
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Return the Base64 part of the
+        Authorization header for a Basic Authentication"""
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header[6:]
