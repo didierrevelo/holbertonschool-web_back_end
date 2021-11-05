7cfaeb46d578aaf5c282c2485c6b57a9fcc5de01
didierrevelo <1829@holbertonschool.com>
2021-11-04 19:37:14 -0500

    api/v1/app.py, api/v1/views/index.py

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -28,6 +28,13 @@ def req_unauthorized(error) -> str:
     return jsonify({"error": "Unauthorized"}), 401
 
 
+@app.errorhandler(403)
+def not_allowed(error) -> str:
+    """ request unauthorized handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
