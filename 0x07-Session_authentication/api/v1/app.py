3e3fe80636c1c516ae3adb876f86864ef497c410
didierrevelo <1829@holbertonschool.com>
2021-11-10 19:19:58 -0500

    api/v1/app.py, api/v1/views/users.py

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -2,14 +2,15 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from os import getenv
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
@@ -23,30 +24,32 @@ elif AUTH_TYPE == "basic_auth":
     auth = BasicAuth()
 
 
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
     """
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def req_unauthorized(error) -> str:
-    """ request unauthorized handler
+@ app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(403)
-def not_allowed(error) -> str:
-    """ request unauthorized handler
+@ app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
     """
     return jsonify({"error": "Forbidden"}), 403
 
 
-@app.before_request
+@ app.before_request
 def before_request() -> str:
-    """ before_request handler"""
+    """ Before Request Handler
+    Requests Validation
+    """
     if auth is None:
         return
 
@@ -58,8 +61,7 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
