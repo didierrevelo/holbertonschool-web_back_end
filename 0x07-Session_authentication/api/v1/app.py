6443a91a6d1eb319691d64bf9ccad5a84e15263e
didierrevelo <1829@holbertonschool.com>
2021-11-11 17:25:18 -0500

    api/v1/app.py

--- 0x07-Session_authentication/api/v1/app.py
+++ 0x07-Session_authentication/api/v1/app.py
@@ -64,7 +64,8 @@ def before_request() -> str:
     if not auth.require_auth(request.path, excluded_paths):
         return
 
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
         abort(401)
 
     current_user = auth.current_user(request)
