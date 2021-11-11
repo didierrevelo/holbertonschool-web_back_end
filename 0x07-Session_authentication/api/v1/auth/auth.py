f90142671a58048d93179b7df1bd3861921ce3c5
didierrevelo <1829@holbertonschool.com>
2021-11-11 17:10:50 -0500

    api/v1/auth/auth.py

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -20,6 +20,7 @@ This class is the template for all authentication
 system you will implement."""
 from flask import request
 from typing import TypeVar, List
+from os import getenv
 
 
 class Auth:
@@ -67,3 +68,17 @@ class Auth:
     def current_user(self, request=None) -> str:
         """current_user method"""
         return None
+
+    def session_cookie(self, request=None):
+        """ session cookie """
+        if request is None:
+            return None
+
+        SESSION_NAME = getenv("SESSION_NAME")
+
+        if SESSION_NAME is None:
+            return None
+
+        session_id = request.cookies.get(SESSION_NAME)
+
+        return session_id
