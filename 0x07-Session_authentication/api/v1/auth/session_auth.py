0ae1c64201467d0b92617e6e99fba5a4f5a5631f
didierrevelo <1829@holbertonschool.com>
2021-11-11 17:47:32 -0500

    api/v1/auth/session_auth.py

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -36,13 +36,12 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id, None)
-    
+
     def current_user(self, request=None):
         """ Returns a User instance based on a cookie value """
         session_id = self.session_cookie(request)
         if session_id is None:
             return None
         user_id = self.user_id_for_session_id(session_id)
-        
 
         return User.get(user_id)
