9bdc2bf834398c8ff5ec232d96d5716795129907
didierrevelo <1829@holbertonschool.com>
2021-11-11 01:12:45 -0500

    api/v1/auth/session_auth.py

--- 0x07-Session_authentication/api/v1/auth/session_auth.py
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -8,11 +8,23 @@ creating a new authentication mechanism:
 validate if everything inherits
 correctly without any overloading
 validate the “switch” by using
-environment variables"""
+environment variables
+
+"""
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """SessionAuth class
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create session """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
