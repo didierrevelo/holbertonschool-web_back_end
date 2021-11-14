b64be939e49bb5561127fd4efbdbbc411a5c7619
didierrevelo <1829@holbertonschool.com>
2021-11-14 14:25:10 -0500

    db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> TypeVar('User'):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """ Adds user to database
         Return: User Object
         """
