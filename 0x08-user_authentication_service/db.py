b1d8b1638a9bc5962be701dfaa5817c25ab599d1
didierrevelo <1829@holbertonschool.com>
2021-11-14 16:09:37 -0500

    db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -47,7 +47,7 @@ class DB:
         """Find user by email
         """
         try:
-            return self._session.query(User).filter_by(**kwargs).one()
+            return self._session.query(User).filter_by(**kwargs).first()
         except NoResultFound:
             raise NoResultFound("No user found")
         except InvalidRequestError:
