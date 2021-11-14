1ac267f4b47e8ff35f268d75e683d1899aa62b53
didierrevelo <1829@holbertonschool.com>
2021-11-14 16:55:32 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -19,18 +19,13 @@ def _hash_password(password: str) -> str:
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
 
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    """Auth.register_user should take mandatory email and password string arguments and return a User object.
-
-    If a user already exist with the passed email, raise a ValueError with the message User <user's email> already exists.
-
-    If not, hash the password with _hash_password, save the user to the database using self._db and return the User object."""
 
     def register_user(self, email: str, password: str) -> User:
         """
