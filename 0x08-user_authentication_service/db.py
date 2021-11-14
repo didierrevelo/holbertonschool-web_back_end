21375dc7185f8f74de063830e23486394d9737ff
didierrevelo <1829@holbertonschool.com>
2021-11-14 16:16:46 -0500

    db.py

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -56,9 +56,10 @@ class DB:
     def update_user(self, user_id: int, **kwargs) -> None:
         """Update user by id
         """
-        _id = self.find_user_by(id=user_id)
+        id = self.find_user_by(id=user_id)
         for key, value in kwargs.items():
-            if not hasattr(_id, key):
+            if not hasattr(id, key):
                 raise ValueError
-            setattr(_id, key, value)
+            setattr(id, key, value)
+
         self._session.commit()
