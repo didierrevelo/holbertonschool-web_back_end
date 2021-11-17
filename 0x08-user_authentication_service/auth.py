7c34695ddc44b21accbbe1bb2d337a17e189ebfe
didierrevelo <1829@holbertonschool.com>
2021-11-17 13:55:07 -0500

    auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -94,3 +94,15 @@ class Auth:
         Destroys the session of the user.
         """
         self._db.update_user(user_id, session_id=None)
+
+    def get_reset_password_token(self, email: str) -> str:
+        """
+        Generates a reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError(f'User {email} does not exist')
+
+        token = _generate_uuid()
+        return token
