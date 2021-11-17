98774b79ffb6432623f30077e548ebacc2067c58
didierrevelo <1829@holbertonschool.com>
2021-11-16 20:38:34 -0500

    update auth

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -39,3 +39,17 @@ class Auth:
             return user
         else:
             raise ValueError(f'User {email} already exists')
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """
+        Checks if the user is valid.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        
+        if bcrypt.checkpw(password.encode(), user.hashed_password):
+            return True
+        
+        return False
