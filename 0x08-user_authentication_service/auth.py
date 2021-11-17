0229038b72572ec739a26248f4e1395578741414
didierrevelo <1829@holbertonschool.com>
2021-11-17 15:15:26 -0500

    auth.py

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -107,7 +107,7 @@ class Auth:
         token = _generate_uuid()
         self._db.update_user(user.id, reset_token=token)
         return token
-    
+
     def update_password(self, reset_token: str, password: str) -> None:
         """
             Updates the password of the user.
@@ -119,4 +119,3 @@ class Auth:
 
         pwd = _hash_password(password)
         self._db.update_user(user.id, hashed_password=pwd, reset_token=None)
-        
