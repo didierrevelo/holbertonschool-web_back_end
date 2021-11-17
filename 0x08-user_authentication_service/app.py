a94042a4b0cc16d11162d3a50b7e2527023d1bf3
didierrevelo <1829@holbertonschool.com>
2021-11-16 20:16:26 -0500

    register user

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 """app.py file"""
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, abort
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -14,6 +16,21 @@ has a single GET route ("/") and
 use flask.jsonify to return a JSON
 payload of the form:"""
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_user() -> str:
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+    except KeyError:
+        abort(400)
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"email": email, "message": "user created"})
+
+    return jsonify({"email": "<registered email>", "message": "user created"})
 
 
 if __name__ == "__main__":
