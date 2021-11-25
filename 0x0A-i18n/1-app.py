5082eb1cde4d92ad85ec98d5a8a103748e0637c3
didierrevelo <1829@holbertonschool.com>
2021-11-25 18:45:33 -0500

    1-app.py

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""First you will setup a basic
+Flask app in 0-app.py. Create a single / route"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__, template_folder="templates")
+babel = Babel(app)
+
+
+class Config(object):
+    """Configure the app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def route_index():
+    """Renders a Basic Template"""
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run()
