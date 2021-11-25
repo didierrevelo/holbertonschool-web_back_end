7cfee67d74a7390af8ebb3f876ace703907a75dc
didierrevelo <1829@holbertonschool.com>
2021-11-25 18:29:15 -0500

    0-app.py

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""First you will setup a basic Flask app in 0-app.py. Create a single / route"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder="templates")
+
+app.route('/', methods=['GET'], strict_slashes=False)
+def route_index():
+    return render_template('0-index.html')
+
+if __name__ == '__main__':
+    app.run()
---
+++ 0x0A-i18n/README.md
@@ -0,0 +1,1 @@
+<h1 align="center">0x0A-i18n</h1>
