5d78cf18bc3d76540de0b2f592c7da7f43030b68
didierrevelo <1829@holbertonschool.com>
2021-11-25 19:24:55 -0500

    4-app.py

---
+++ 0x0A-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""First you will setup a basic
+Flask app in 0-app.py. Create a single / route"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from flask import request
+
+
+app = Flask(__name__, template_folder="templates")
+babel = Babel(app)
+
+
+class Config(object):
+    """Configure the app"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def route_index():
+    """Renders a Basic Template"""
+    return render_template('3-index.html')
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Locale Selector"""
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == '__main__':
+    app.run()
