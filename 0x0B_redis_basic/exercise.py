fc519baf834f38aa0448df2d7fb47f2663eaa092
didierrevelo <1829@holbertonschool.com>
2021-11-30 22:14:01 -0500

    get redis

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Writing strings to Redis"""
 from redis import Redis
-from typing import Union
+from typing import Union, Optional, Callable
 from uuid import uuid4
 
 
@@ -20,7 +20,30 @@ class Cache:
         Returns:
             str: key
         """
-        ramdon_key = str(uuid4())
-        self._redis.set(ramdon_key, data)
+        random_key = str(uuid4())
+        self._redis.set(random_key, data)
 
-        return ramdon_key
+        return random_key
+
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ Reading from Redis and recovering original type """
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+
+        return value
+
+    def get_str(self, key: str) -> str:
+        """ Parameterizes a value from redis to str """
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        """ Parameterizes a value from redis to int """
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
