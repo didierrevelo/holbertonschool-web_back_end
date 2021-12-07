993f7dd6411641ae9ec78eeddffcad42640943ea
didierrevelo <1829@holbertonschool.com>
2021-12-07 17:09:57 -0500

    9-insert_school.py

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -7,5 +7,4 @@ in a collection based on kwargs"""
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new document"""
     insert = mongo_collection.insert(**kwargs)
-    
     return insert
