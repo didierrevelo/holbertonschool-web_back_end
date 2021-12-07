ee94c5e3b5689d4a9e10ceeba3089e51736a37b4
didierrevelo <1829@holbertonschool.com>
2021-12-07 17:24:20 -0500

    10-update_topics.py

--- 0x0D-NoSQL/10-update_topics.py
+++ 0x0D-NoSQL/10-update_topics.py
@@ -10,4 +10,4 @@ def update_topics(mongo_collection, name, topics):
     query = {"name": name}
     new_values = {"$set": {"topics": topics}}
 
-    mongo_collection.update_many(query, new_values)
+    mongo_collection.update(query, new_values)
