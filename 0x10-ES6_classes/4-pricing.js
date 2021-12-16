14d96ec3f963a8482cd147e4c572e7e5c15b202c
didierrevelo <1829@holbertonschool.com>
2021-12-16 14:53:18 -0500

    4-pricing.js

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('Currency must be a Currency object');
+    }
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return (amount * conversionRate);
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} ${this.currency.code}`;
+  }
+}
