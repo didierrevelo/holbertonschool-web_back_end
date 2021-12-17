5eb7f5c5a78ca2530181dca0ae6124afb967d07f
didierrevelo <1829@holbertonschool.com>
2021-12-16 19:00:54 -0500

    9-hoisting.js

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -17,10 +17,10 @@ const class2019 = new HolbertonClass(2019, 'San Francisco');
 const class2020 = new HolbertonClass(2020, 'San Francisco');
 
 export class StudentHolberton {
-  constructor(firstName, lastName) {
+  constructor(firstName, lastName, holbertonClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = HolbertonClass;
+    this._holbertonClass = holbertonClass;
   }
 
   get fullName() {
