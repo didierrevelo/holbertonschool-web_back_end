efded8b458e768a85d6b9ee6a50f1a88c9b4eda3
didierrevelo <1829@holbertonschool.com>
2021-12-16 19:08:01 -0500

    9-hoisting.js

--- 0x10-ES6_classes/9-hoisting.js
+++ 0x10-ES6_classes/9-hoisting.js
@@ -28,13 +28,14 @@ export class StudentHolberton {
   }
 
   get holbertonClass() {
-    return this.holbertonClass;
+    return this._holbertonClass;
   }
 
   get fullStudentDescription() {
     return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
   }
 }
+
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
@@ -42,4 +43,5 @@ const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
+
 export default listOfStudents;
