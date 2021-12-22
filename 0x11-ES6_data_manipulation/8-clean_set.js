1bf145cfadb262747b687837301786af94f3e6e9
didierrevelo <1829@holbertonschool.com>
2021-12-21 23:07:42 -0500

    8-clean_set.js

---
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,18 @@
+export default function cleanSet(set, startString) {
+  const string = [];
+
+  if (
+    typeof set !== 'object'
+    || typeof startString !== 'string'
+    || startString.length === 0
+  ) {
+    return '';
+  }
+
+  for (const item of set) {
+    if (item && item.startsWith(startString)) {
+      string.push(item.slice(startString.length));
+    }
+  }
+  return string.join('-');
+}
