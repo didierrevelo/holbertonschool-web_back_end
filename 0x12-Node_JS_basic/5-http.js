0420201865c637976802bf169693fd6573bd8eeb
didierrevelo <1829@holbertonschool.com>
2021-12-28 17:20:42 -0500

    5-http.js

---
+++ 0x12-Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const data = args[0];
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer(async (req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    try {
+      const list = await countStudents(data);
+      res.end(`${list.join('\n')}`);
+    } catch (error) {
+      res.write(error);
+      res.end();
+    }
+  }
+  res.statusCode = 404;
+  res.end();
+});
+app.listen(port, hostname, () => {
+  // console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
