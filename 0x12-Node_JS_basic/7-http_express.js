b09b7f6c5fea1a6dac348eca7886a2e81784c2f6
didierrevelo <1829@holbertonschool.com>
2021-12-28 17:30:02 -0500

    7-http_express.js

---
+++ 0x12-Node_JS_basic/7-http_express.js
@@ -0,0 +1,29 @@
+const express = require('express');
+
+const args = process.argv.slice(2);
+const countStudents = require('./3-read_file_async');
+
+const data = args[0];
+const app = express();
+const hostname = '127.0.0.1';
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  const message = 'This is the list of our students\n';
+  try {
+    const list = await countStudents(data);
+    res.send(`${message}${list.join('\n')}`);
+  } catch (error) {
+    res.send(error);
+  }
+});
+
+app.listen(port, hostname, () => {
+  // console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+module.exports = app;
