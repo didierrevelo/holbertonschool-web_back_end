cee9a7ebf20ad9eb3414bd2e0a5d255731f1b6b3
didierrevelo <1829@holbertonschool.com>
2022-01-06 18:07:51 -0500

    2-calcul_chai.test.js

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,5 +1,5 @@
 const chai = require('chai');
-const calculateNumber = require("./1-calcul.js");
+const calculateNumber = require('./2-calcul_chai');
 
 describe('calculateNumber', function () {
   describe('two integrer', () => {
